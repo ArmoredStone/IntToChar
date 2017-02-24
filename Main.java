b0f3758c1a91f25b2cb0eda0ab2788488f94b40f
Alex Aheyev <ah.alesha@gmail.com>
2017-02-25 01:41:45 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,13 @@
+package pkg4;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 15;
+        System.out.println(intToChar(a));
+    }
+    public static char intToChar(int integer){
+    return (char) integer;
+}
+    
+}
